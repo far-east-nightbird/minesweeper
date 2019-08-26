0556eea1fc647884695c149c096fec7fb8493fae
far-east-nightbird <liliya.kupriyenko@gmail.com>
2019-08-26 10:29:33 +0200

    Update Spielfeld.java

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -1,100 +1,83 @@
+import java.util.Random;
 
-import java.util.*;
+public class Spielbrett {
 
-public class Spielfeld {
-	private String[][] spielfeldVerdeckt;
-	private String[][] spielfeldAnzeige;
-	private static final int zeile = 8;
-	private static final int spalte = 8;
-	spielfeldVerdeckt = new String[zeile][spalte];
+	int zeilen = 8;
+	int spalten = 8;
+	int anzahlBomben = 4;
+	private Zelle[][] zellen;
+	boolean spielzuende;
 	
-	private Random rand = new Random();
-	private int anzahlBomben = 4;
-	private String flagge = "!";
-	private String bombe = "*";
+	Random zufall = new Random();
+	
+	
+	public void generiereSpielbrett() {
 
-	public void initiiereSpielfeld() {
+		zellen = new Zelle[zeilen][spalten];
 		
-		for(int i = 0; i < zeile; i++) {
-			for(int j = 0; j < spalte; j++) {
-				spielfeldVerdeckt[i][j] = "_" ;
-			}
-		}
-	}
-	
-	public void verteileBomben() {
-		for(int i=0; i < anzahlBomben; i++) {
-			int zufaelligeZeile = rand.nextInt(8);
-			int zufaelligeSpalte = rand.nextInt(8);
-			spielfeldVerdeckt[zufaelligeZeile][zufaelligeSpalte] = "*";
-		}
-	} 
-	
-	public void verteileZahlen() {
-		
-		for(int i = 0; i < zeile; i++) {
-			for(int j = 0; j < (spalte +1); j++) {
-				int bombenAnzeige = 0;
-				if(spielfeldVerdeckt[i][j] != "*") {
-					
-					//Die benachbarten Zellen überprüfen
-					for(int k=-1; i<4; i++) {
-						for(int l=-1; i<4; i++) {
-							if (spielfeldVerdeckt[i+k][j+l] == "*") {
-								bombenAnzeige++;
-						}
-						
-						}
-						spielfeldVerdeckt[i][j] = Integer.toString(bombenAnzeige);
-					}
-				}
-			}
-		}
-	}
-		
-	
-	
-	public void zeigeSpielfeld() {
-		//Spalten
-		System.out.print(" ");
-		for(int i = 0; i < zeile; i++) {
-			System.out.print(i + " ");
-		}		
-		System.out.println();
-		for(int i = 0; i < zeile; i++) {
-			System.out.print(i);
-			for(int j = 0; j < (spalte +1); j++) {
-				System.out.print(" ");
-				spielfeldVerdeckt[i][j] = " "; //Füllt die Array mit " "
+		for(int i = 0; i < zeilen; i++) {
+			for(int j=0; j < spalten; j++) {
+				zellen[i][j] = new Zelle();
 			}
 			System.out.println();
 		}
+		System.out.println();
+	}
+	
+	public void verteileMinen() {
+		for(int i=0; i<4; i++) {
+			int randX = zufall.nextInt(zeilen);
+	        int randY = zufall.nextInt(spalten);
+	        
+	        Zelle zelle = new Zelle();
+	        zelle = zellen[randY][randY];
+	        
+	        System.out.print(randX + " " + randY + " ");
+	        
+	        if (!zellen[randX][randY].istMine()) {
+	            zelle.setzeMine(true);
+	            ersetzeZelle(randX,randY);
+	           
+	            
+		}
+		}
+}
+	public void ersetzeZelle(int x, int y) {
+		zellen[x][y] = new Zelle();
+		Zelle zelle = zellen[x][y];
 	}
 	
 	
+	public void oeffneFeld(){
+		if(Eingabe.aOderM == 'O') {
+	Zelle zelle = zellen[Eingabe.eingabeZeile][Eingabe.eingabeSpalte];
+	if(zelle.istMine()) {
+		System.out.print("*");
+	}
+}	
+	}
 	
-	/**
-	 * public String druckeZeileSpalte() {
-		//Spalten
-		String brett = "";
-		for(int i = 0; i < zeile; i++) {
-			brett = java.lang.String.valueOf(i) + " ";
-		}
-	
-	
+	public void zeigeSpielfeld() {
 		
-		for(int i = 0; i < zeile; i++) {
-			brett = java.lang.String.valueOf(i);
-			for(int j = 0; j < spalte; j++) {
-				brett += spielfeld[i][j];
-			}
-		}
-		return brett;
-	}*/
-
-	
-	
-	
+	 for (int i = 0; i < zeilen; i++) {
+	            for (int j = 0; j < spalten; j++) {
+	                Zelle zelle = zellen[i][j];
+	                if(zelle.istMine())
+	                {
+	                	 System.out.print("[]");
+	                }
+	               
+	            }
+	            System.out.println();
+	 }
 	}
+	/*public void zeigeSpielfeld() {
+		for(int i = 0; i < zeilen; i++) {
+			for(int j=0; j < spalten; j++) {
+				zellen[i][j];
+			}
+			System.out.println();
+		}
+	}*/
 	
-
+}
